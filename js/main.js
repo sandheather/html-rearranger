614ccd1e5a2c5a048c79601a506bcf263fb274c3
sandheather <heather.e.sanders22@gmail.com>
2016-03-11 17:41:58 -0500

    variables changed and statement added

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var $newHeadingBelow = $('<h1>');
+var $newUl1 = $('<ul>');
+var $newHeadingAbove = $('<h1>');
+var $newUl2 = $('<ul>');
+
+$newHeadingBelow.html('Below ground veggies');
+$('body').append($newHeadingBelow);
+$('body').append($newUl1);
+
+$newHeadingAbove.html('Above ground veggies');
+$('body').append($newHeadingAbove);
+$('body').append($newUl2);
+
+$('ul li').each(function () {
+  if ($(this).hasClass('below')) {
+    $newUl1.append($(this));
+  } else {
+    $newUl2.append($(this));
+  }
+});
